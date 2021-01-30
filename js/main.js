12ec032512c71670ebab029a650e352ff5ce3275
ILIDAN <mrster03@gmail.com>
2021-01-30 21:18:12 +0300

    Написал Функцию описанную в ТЗ к заданию.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const randomFloat = function (from, to, signsQuantity) {
+  if(from > to || from == to) {
+    return 'Значение "от" не может быть больше или равнятся значению "до"!';
+  }
+
+  if (from < 0 || to < 0 ) {
+    return 'Значения не могут быть меньше нуля!';
+  }
+
+  let rand = from + Math.random() * (to - from);
+  return rand.toFixed(signsQuantity);
+
+}
+
+console.log(randomFloat(30, 203, 4));
