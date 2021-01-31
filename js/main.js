5335450f3fbf335cc4b832b9f2da4e18c9a88731
ILIDAN <mrster03@gmail.com>
2021-01-31 11:12:02 +0300

    Поменял return в условных операторах функции на throw new Error.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const randomFloat = function (from, to, signsQuantity) {
   if(from > to || from == to) {
-    return 'Значение "от" не может быть больше или равнятся значению "до"!';
+    throw new Error('Значение "от" не может быть больше или равнятся значению "до"!');
   }
 
   if (from < 0 || to < 0 ) {
-    return 'Значения не могут быть меньше нуля!';
+    throw new Error('Значения не могут быть меньше нуля!');
   }
 
   let rand = from + Math.random() * (to - from);
