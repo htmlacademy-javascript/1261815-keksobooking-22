816a83d3d2dfe36c9f39fa20b9ce81d841a03df7
ILIDAN <mrster03@gmail.com>
2021-02-26 19:25:49 +0300

    Внёс правки по замечаниям наставника.

--- js/random-array-element.js
+++
@@ -1,7 +0,0 @@
-import {getRandomNumber} from './random-number.js';
-
-const getRandomArrayElement = (elements) => {
-  return elements[getRandomNumber(0, elements.length - 1)];
-}
-
-export {getRandomArrayElement};
